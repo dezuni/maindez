52dfba96555a2f83681ee2b088dd2fc884565cf4
dez-bme <dez.bme.stds@gmail.com>
2025-04-11 00:34:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Current app version - update with each deployment
-const APP_VERSION = '25.04.110';
+const APP_VERSION = '25.04.11.0';
 
 // Check for version update
 function checkVersion() {
