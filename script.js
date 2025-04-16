168cfe8dd0317f1e6e6646cf9d779536f22be69b
dezuni <unidez.ir@gmail.com>
2025-04-16 12:50:59 +0330

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Current app version - update with each deployment
-const APP_VERSION = '25.04.15.2';
+const APP_VERSION = '25.04.16.1';
 
 // check version updates
 async function checkVersion() {
